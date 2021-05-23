bd9033331177ba9b7d599c88365bf0dc0a98d03b
Ignacio Francos Koch <67810079+nachokoch@users.noreply.github.com>
2021-05-23 18:13:15 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Header from "./Components/Header";
+import styled from "styled-components";
+import Formulario from "./Components/Formulario";
+import Resumen from "./Components/Resumen";
+import Resultado from "./Components/Resultado";
+import Spinner from "./Components/Spinner";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
+
+function App() {
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: "",
+      year: "",
+      plan: "",
+    },
+  });
+
+  const [cargando, guardarCargando] = useState(false);
+
+  const { cotizacion, datos } = resumen;
+
+  return (
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />
+      <ContenedorFormulario>
+        <Formulario
+          guardarResumen={guardarResumen}
+          guardarCargando={guardarCargando}
+        />
+        {cargando ? <Spinner /> : null}
+        <Resumen datos={datos} />
+
+        {!cargando ? <Resultado cotizacion={cotizacion} /> : null}
+      </ContenedorFormulario>
+    </Contenedor>
+  );
+}
+
+export default App;
